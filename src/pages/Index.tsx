a9fe5bd7b6ac290e4359f10c8075100d2b44db4b
TeacherJob <mentisgatelearningpvtlimited@gmail.com>
2025-07-19 17:27:17 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1157,7 +1157,7 @@ const Index = () => {
       <HowWeWork />
       <ForEmployers />
       <JobSearchSection />
-      <TestimonialsSection />
+{/*       <TestimonialsSection /> */}
       <StatsSection />
       <Footer />
     </div>
